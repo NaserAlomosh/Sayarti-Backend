ef8a4b218b8cf3ed34dd53a83119ecb9a68a6ea5
Naser Alomosh <naseralomosh1@gmail.com>
2026-08-21 15:46:36 +0300

    Fix

--- src/main/resources/db/migration/V3__add_google_identity.sql
+++ src/main/resources/db/migration/V3__add_google_identity.sql
@@ -1,3 +1,5 @@
-ALTER TABLE users ALTER COLUMN password_hash VARCHAR(100) NULL;
-ALTER TABLE users ADD google_subject VARCHAR(255) NULL;
-CREATE UNIQUE INDEX uq_users_google_subject ON users (google_subject) WHERE google_subject IS NOT NULL;
+ALTER TABLE users
+ALTER COLUMN password_hash VARCHAR(100) NULL;
+
+ALTER TABLE users
+    ADD google_subject VARCHAR(255) NULL;
