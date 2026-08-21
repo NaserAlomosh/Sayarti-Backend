aba6568b2a634bf3497ce9f7c568857ed924fa9e
Naser Alomosh <naseralomosh1@gmail.com>
2026-08-21 12:53:48 +0300

    First Commit

---
+++ src/main/java/com/smart/sayarti/SayartiApplication.java
@@ -0,0 +1,13 @@
+package com.smart.sayarti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayartiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SayartiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smart/sayarti/SayartiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.sayarti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SayartiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
